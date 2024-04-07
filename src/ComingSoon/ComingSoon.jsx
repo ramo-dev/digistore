29e4ffce24ba475397bb2dca36f7a3d975c34d0f
ramo-dev <annuar.dev@gmail.com>
2024-04-07 16:54:47 +0300

    Coming Soon

--- src/ComingSoon/ComingSoon.jsx
+++ src/ComingSoon/ComingSoon.jsx
@@ -1,55 +1,92 @@
+import { useState } from "react";
+import { toast, Toaster } from "sonner";
+import { Flex } from "antd";
+import "./ComingSoonStyles.css";
+import { db } from "../utils/firebase";
 import Logo from "../assets/digistoreAssets/png/logo-no-background.png";
-import {toast, Toaster} from 'sonner'
-import {Flex} from 'antd'
-import './ComingSoonStyles.css'
-import { useEffect } from "react";
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
+
 const ComingSoon = () => {
-    function handleFormsubmit(e){
-        e.preventDefault()
-        const email = e.target[0].value
-        console.log(email)
-        email ? toast.success(`Thank You for Subscribing, you will be notified when we launch`, { position: "top-right" }) : toast.error("Please enter a valid email", { position: "top-right" })
+  const [email, setEmail] = useState("");
+
+  const handleFormsubmit = async (e) => {
+    e.preventDefault();
+    const inputEmail = e.target[0].value;
+
+    if (inputEmail) {
+      const emailExists = await emailAlreadyExists(inputEmail);
+      if (!emailExists) {
+        await addSubscriber(inputEmail);
+        toast.success(
+          `Thank you for subscribing! You will be notified when we launch.`
+        );
+      } else {
+        toast.error(`This email is already subscribed.`);
+      }
+    } else {
+      toast.error(`Please enter a valid email.`);
     }
-    return (
-      <>
-        <Toaster richColors/>
+
+    e.target.reset();
+  };
+
+  const emailAlreadyExists = async (inputEmail) => {
+    const q = query(
+      collection(db, "subscribers"),
+      where("email", "==", inputEmail)
+    );
+    const querySnapshot = await getDocs(q);
+    return !querySnapshot.empty;
+  };
+
+  const addSubscriber = async (inputEmail) => {
+    try {
+      await addDoc(collection(db, "subscribers"), { email: inputEmail });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <Toaster richColors position="top-right" />
+      <Flex
+        className="comingSoon"
+        vertical
+        align="center"
+        justify="center"
+        gap="3rem"
+      >
+        <img
+          src={Logo}
+          alt="DigiStore - Premier Platform for Talented Individuals"
+        />
         <Flex
-          className="comingSoon"
           vertical
           align="center"
           justify="center"
-          gap="3rem"
+          style={{ textAlign: "center" }}
         >
-          <img
-            src={Logo}
-            alt="DigiStore - Premier Platform for Talented Individuals"
-          />
-          <Flex
-            vertical
-            align="center"
-            justify="center"
-            style={{ textAlign: "center" }}
-          >
-            <h1>Coming Soon</h1>
-            <p>Sign Up to Our newsletter to get notified when we launch </p>
-          </Flex>
-          <form action="" className="subscribeForm" onSubmit={handleFormsubmit}>
-            <input
-              type="email"
-              className="input"
-              placeholder="Enter your email..."
-              required
-            />
-            <button className="btn">Subscribe</button>
-          </form>
+          <h1>Coming Soon</h1>
+          <p>Sign Up to Our newsletter to get notified when we launch </p>
         </Flex>
-        <footer>
-          <Flex justify="center" style={{ padding: "1rem" }}>
-            {new Date().getFullYear()} &copy; DigiStore. All Rights Reserved.
-          </Flex>
-        </footer>
-      </>
-    );
-}
- 
+        <form action="" className="subscribeForm" onSubmit={handleFormsubmit}>
+          <input
+            type="email"
+            className="input"
+            placeholder="Enter your email..."
+            required
+          />
+          <button className="btn">Subscribe</button>
+        </form>
+      </Flex>
+      <footer>
+        <Flex justify="center" style={{ padding: "1rem" }}>
+          {new Date().getFullYear()} &copy; DigiStore. All Rights Reserved.
+        </Flex>
+      </footer>
+    </>
+  );
+};
+
 export default ComingSoon;
--- src/utils/firebase.jsx
+++ src/utils/firebase.jsx
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
+import {getAuth} from 'firebase/auth'
+import {getFirestore} from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,10 +20,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-
-
+// initialize auth and db
+export const db = getFirestore(app);
+export const account = getAuth(app)
 
 
 
